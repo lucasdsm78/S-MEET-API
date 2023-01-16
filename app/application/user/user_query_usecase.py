838c5d76d32cdf85de09243123d6cb94402be2f2
lucasdsm78 <66834518+lucasdsm78@users.noreply.github.com>
2023-01-16 10:36:30 +0100

    get profile

--- app/application/user/user_query_usecase.py
+++ app/application/user/user_query_usecase.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import Optional
 
 from pydantic import BaseModel
 
 from app.application.user.user_query_model import UserReadModel
+from app.domain.user.exception.user_exception import UserNotFoundError
 from app.domain.user.repository.user_repository import UserRepository
 
 
@@ -10,6 +12,10 @@ class UserQueryUseCase(ABC):
 
     @abstractmethod
     def fetch_users(self) -> dict:
+        raise NotImplementedError
+
+    @abstractmethod
+    def fetch_user_by_id(self, user_id: int) -> Optional[UserReadModel]:
         raise NotImplementedError
 
 
@@ -29,4 +35,14 @@ class UserQueryUseCaseImpl(UserQueryUseCase, BaseModel):
 
         except Exception as e:
             raise
+
+    def fetch_user_by_id(self, user_id: int) -> Optional[UserReadModel]:
+        try:
+            user = self.user_repository.find_by_id(user_id)
+            if user is None:
+                raise UserNotFoundError
+        except:
+            raise
+
+        return UserReadModel.from_entity(user)
 
--- app/presentation/user/user_router.py
+++ app/presentation/user/user_router.py
@@ -3,10 +3,12 @@ from fastapi import APIRouter, Depends, HTTPException, status, Query
 from app.application.user.user_command_model import UserCreateResponse, UserCreateModel, UserLoginResponse, \
     UserLoginModel
 from app.application.user.user_command_usecase import UserCommandUseCase
+from app.application.user.user_query_model import UserReadModel
 from app.application.user.user_query_usecase import UserQueryUseCase
 from app.dependency_injections import user_command_usecase, user_query_usecase
-from app.domain.user.exception.user_exception import UserEmailAlreadyExistsError, UsersNotFoundError
-from app.presentation.user.user_error_message import ErrorMessageUserEmailAlreadyExists, ErrorMessageUsersNotFound
+from app.domain.user.exception.user_exception import UserEmailAlreadyExistsError, UsersNotFoundError, UserNotFoundError
+from app.presentation.user.user_error_message import ErrorMessageUserEmailAlreadyExists, ErrorMessageUsersNotFound, \
+    ErrorMessageUserNotFound
 
 router = APIRouter(
     tags=['user']
@@ -82,3 +84,32 @@ async def login(
         user_command_usecase: UserCommandUseCase = Depends(user_command_usecase),
 ):
     return user_command_usecase.login(user_login_model).for_login()
+
+
+@router.get(
+    "/user/profile/{id}",
+    response_model=UserReadModel,
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "model": ErrorMessageUserNotFound,
+        },
+    },
+)
+async def get_profile(
+        user_id: int,
+        user_query_usecase: UserQueryUseCase = Depends(user_query_usecase),
+):
+    try:
+        user = user_query_usecase.fetch_user_by_id(user_id)
+    except UserNotFoundError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=e.message,
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+    return user
