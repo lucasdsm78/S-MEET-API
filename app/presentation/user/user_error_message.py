27f4f098cf00a89df10aff20ed086e5105375358
lucasdsm78 <dasilvamarqueslucas@gmail.com>
2023-04-12 10:08:16 +0200

    add command and query for messages

--- app/presentation/user/user_error_message.py
+++ app/presentation/user/user_error_message.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 
+from app.domain.chat.message.exception.message_exception import MessagesNotFoundError
 from app.domain.user.exception.user_exception import UserEmailAlreadyExistsError, UsersNotFoundError, UserNotFoundError
 
 
@@ -13,3 +14,7 @@ class ErrorMessageUsersNotFound(BaseModel):
 
 class ErrorMessageUserNotFound(BaseModel):
     detail: str = Field(example=UserNotFoundError.message)
+
+
+class ErrorMessagesRoomNotFound(BaseModel):
+    detail: str = Field(example=MessagesNotFoundError.message)
